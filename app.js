898819f172cdb2587bd68e1257d826d78d09578a
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2018-12-20 14:28:31 -0800

    added mongodb and Review model

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+});
+
 // app.get('/', (req, res) => {
 //     res.render('home', { msg: 'Handlebars are Cool!' });
 // })
@@ -15,14 +23,20 @@ app.listen(3000, () => {
 })
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II" },
-    { title: "Awesome Movie", movieTitle: "Titanic" },
-    { title: "Move of the Year", movieTitle: "Undercover Brother" },
-    { title: "Terrible Movie", movieTitle: "Ninja Assassin" }
-  ]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" },
+//     { title: "Move of the Year", movieTitle: "Undercover Brother" },
+//     { title: "Terrible Movie", movieTitle: "Ninja Assassin" }
+//   ]
   
   // INDEX
   app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+    Review.find()
+      .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+      })
+      .catch(err => {
+        console.log(err);
+      })
   })
