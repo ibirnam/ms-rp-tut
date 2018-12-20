8869dd47e70535e99bcca8ed787672dbeec92117
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2018-12-20 14:16:14 -0800

    users can see all (mock) reviews

--- app.js
+++ app.js
@@ -6,10 +6,21 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-})
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Handlebars are Cool!' });
+// })
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
 })
+
+// OUR MOCK ARRAY OF PROJECTS
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+  ]
+  
+  // INDEX
+  app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
+  })
