fa61c42ea3dba03b487143a8cd1b1b330acd7f1a
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2018-12-20 15:21:19 -0800

    users can see single reviews

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const Review = mongoose.model('Review', {
     title: String,
     description: String,
-    movieTitle: String
+    movieTitle: String,
+    rating: Number
 });
 
 // app.get('/', (req, res) => {
@@ -56,7 +57,16 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
     Review.create(req.body).then((review) => {
       console.log(review);
-      res.redirect('/');
+      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
+    }).catch((err) => {
+      console.log(err.message);
+    })
+})
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+      res.render('reviews-show', { review: review })
     }).catch((err) => {
       console.log(err.message);
     })
