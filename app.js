274cf947701ecedccefe19d9c19cbc7be4071e75
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2018-12-20 14:40:57 -0800

    users can create reviews

--- app.js
+++ app.js
@@ -9,8 +9,14 @@ app.set('view engine', 'handlebars');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 
+// INITIALIZE BODY-PARSER AND ADD IT TO APP
+const bodyParser = require('body-parser');
+// The following line must appear AFTER const app = express() and before your routes!
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const Review = mongoose.model('Review', {
     title: String,
+    description: String,
     movieTitle: String
 });
 
@@ -30,13 +36,28 @@ app.listen(3000, () => {
 //     { title: "Terrible Movie", movieTitle: "Ninja Assassin" }
 //   ]
   
-  // INDEX
-  app.get('/', (req, res) => {
-    Review.find()
-      .then(reviews => {
-        res.render('reviews-index', { reviews: reviews });
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  })
+// INDEX
+app.get('/', (req, res) => {
+Review.find()
+    .then(reviews => {
+    res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+    console.log(err);
+    })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+      console.log(review);
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err.message);
+    })
+})
