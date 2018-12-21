b6fc74ab53c8ce9c053131364fda962bc8b7e9c0
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2018-12-21 11:34:19 -0800

    delete comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,13 +3,22 @@ const Comment = require('../models/comment');
 module.exports = (app) => {
 
     // CREATE Comment
-app.post('/reviews/comments', (req, res) => {
-    Comment.create(req.body).then((comment) => {
-      console.log(comment)
-      res.redirect(`/reviews/${comment.reviewId}`);
-    }).catch((err) => {
-      console.log(err.message);
+    app.post('/reviews/comments', (req, res) => {
+        Comment.create(req.body).then((comment) => {
+        console.log(comment)
+        res.redirect(`/reviews/${comment.reviewId}`);
+        }).catch((err) => {
+        console.log(err.message);
+        });
     });
-  });
-  
+
+    // DELETE
+    app.delete('/reviews/comments/:id', function (req, res) {
+        console.log("DELETE comment")
+        Comment.findByIdAndRemove(req.params.id).then((comment) => {
+            res.redirect(`/reviews/${comment.reviewId}`);
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
 }
