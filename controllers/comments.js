d45db3cd102c1049f358fa0426b3badaf2ba6b31
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-01-28 11:59:20 -0800

    chapter 4: updated CRUD comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,20 +3,20 @@ const Comment = require('../models/comment');
 module.exports = (app) => {
 
     // CREATE Comment
-    app.post('/reviews/comments', (req, res) => {
+    app.post('/movies/:movieid/reviews/:reviewId/comments', (req, res) => {
         Comment.create(req.body).then((comment) => {
-        console.log(comment)
-        res.redirect(`/reviews/${comment.reviewId}`);
+            console.log(comment)
+            res.redirect(`/movies/${req.body.movieId}/reviews/${comment.reviewId}`);
         }).catch((err) => {
-        console.log(err.message);
+            console.log(err.message);
         });
     });
 
     // DELETE
-    app.delete('/reviews/comments/:id', function (req, res) {
+    app.delete('/movies/:movieId/reviews/:reviewId/comments/:id', function (req, res) {
         console.log("DELETE comment")
         Comment.findByIdAndRemove(req.params.id).then((comment) => {
-            res.redirect(`/reviews/${comment.reviewId}`);
+            res.redirect(`/movies/${req.body.movieId}/reviews/${comment.reviewId}`);
         }).catch((err) => {
             console.log(err.message);
         })
