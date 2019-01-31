9ca86f9904284bb216b5e540f0538048bf0559da
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-01-31 14:45:24 -0800

    can delete newly created comments without refresh

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,5 +1,31 @@
 // scripts.js
 
+function deleteComment() {
+    document.querySelectorAll('.delete-comment').forEach((commentElement) => {
+        commentElement.addEventListener('click', (e) => {
+            console.log("click!")
+            console.log("e: ", e);
+            let commentId = e.target.getAttribute('data-comment-id')
+            let reviewId = e.target.getAttribute('data-comment-reviewId')
+            let movieId = e.target.getAttribute('data-comment-movieId')
+            console.log("commentId: ", commentId);
+            console.log("reviewId: ", reviewId);
+            console.log("movieId: ", movieId);
+            axios.delete(`/movies/${movieId}/reviews/${reviewId}/comments/${commentId}`)
+                .then(response => {
+                    console.log(response)
+                    console.log("commentId AFTER delete: ", commentId);
+                    comment = document.getElementById(commentId)
+                    comment.parentNode.removeChild(comment); // OR comment.style.display = 'none';
+                })
+                .catch(error => {
+                    console.log(error)
+                    alert('There was an error deleting this comment.')
+                });
+        })
+    })
+}
+
 if (document.getElementById('new-comment')) {
     // listen for a form submit event
     document.getElementById('new-comment').addEventListener("submit", e => {
@@ -27,47 +53,25 @@ if (document.getElementById('new-comment')) {
                 console.log('movieId: ', movieId);
                 // display the data as a new comment on the page
                 document.getElementById('comments').insertAdjacentHTML('afterbegin',
-                    `<div class="card" id="{{response.data.comment._id}}">
+                    `<div class="card" id="${response.data.comment._id}">
                             <div class="card-block">
                                 <h4 class="card-title">${response.data.comment.title}</h4>
                                 <p class="card-text">${response.data.comment.content}</p>
-                                <button class="btn btn-link delete-comment" data-comment-id="{{response.data.comment._id}}" data-comment-reviewId="{{response.data.comment.reviewId}}" data-comment-movieId="{{movieId}}"">Delete</button>
+                                <button class="btn btn-link delete-comment" data-comment-id="${response.data.comment._id}" data-comment-reviewId="${response.data.comment.reviewId}" data-comment-movieId="${movieId}"">Delete</button>
                             </div>
                         </div>`
                 );
+
+                deleteComment();
+
             })
     });
 }
 
-// function addDeleteCommentEvent() {
+
 if (document.querySelectorAll('.delete-comment')) {
-    document.querySelectorAll('.delete-comment').forEach((commentElement) => {
-        commentElement.addEventListener('click', (e) => {
-            console.log("click!")
-            console.log("e: ", e);
-            let commentId = e.target.getAttribute('data-comment-id')
-            let reviewId = e.target.getAttribute('data-comment-reviewId')
-            let movieId = e.target.getAttribute('data-comment-movieId')
-            console.log("commentId: ", commentId);
-            console.log("reviewId: ", reviewId);
-            console.log("movieId: ", movieId);
-            axios.delete(`/movies/${movieId}/reviews/${reviewId}/comments/${commentId}`)
-                .then(response => {
-                    console.log(response)
-                    console.log("commentId AFTER delete: ", commentId);
-                    comment = document.getElementById(commentId)
-                    comment.parentNode.removeChild(comment); // OR comment.style.display = 'none';
-                })
-                .catch(error => {
-                    console.log(error)
-                    alert('There was an error deleting this comment.')
-                });
-        })
-    })
+    deleteComment();
 }
-// setTimeout(addDeleteCommentEvent, 100);
-// }
-// addDeleteCommentEvent();
 
 if (document.querySelectorAll('.delete-review')) {
     document.querySelectorAll('.delete-review').forEach((reviewElement) => {
@@ -78,8 +82,8 @@ if (document.querySelectorAll('.delete-review')) {
             console.log("reviewId: ", reviewId);
 
             axios.delete(`/admin/reviews/${reviewId}`)
-                .then(response => { 
-                    console.log("response: ",response);
+                .then(response => {
+                    console.log("response: ", response);
                     review = document.getElementById(reviewId)
                     review.parentNode.removeChild(review); // OR review.style.display = 'none';
                 }).catch((err) => {
